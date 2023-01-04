dc8f8871391a734cc846246ba0474b62d059b6d5
Justin.Mihklep <justinmihklep@msn.com>
2023-01-04 18:50:46 +0200

    First commit

---
+++ View.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+
+class View(Tk):
+
+    def __init__(self, controller, model):
+        super().__init__()
+        self.controller = controller
+        self.model = model
+        self.userinput = StringVar()
+
+        # TODO Fonts
+        # Window properties
+        self.geometry('515x200')
+        self.title('Hangman')
+        self.center(self)
+
+    def main(self):
+        self.mainloop()
+
+    @staticmethod
+    def center(win):
+        """
+        https://stackoverflow.com/questions/3352918/how-to-center-a-window-on-the-screen-in-tkinter
+        centers a tkinter window
+        :param win: the main window or Toplevel window to center
+        """
+        win.update_idletasks()
+        width = win.winfo_width()
+        frm_width = win.winfo_rootx() - win.winfo_x()
+        win_width = width + 2 * frm_width
+        height = win.winfo_height()
+        titlebar_height = win.winfo_rooty() - win.winfo_y()
+        win_height = height + titlebar_height + frm_width
+        x = win.winfo_screenwidth() // 2 - win_width // 2
+        y = win.winfo_screenheight() // 2 - win_height // 2
+        win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        win.deiconify()
