3a8c160bd1e30f8d010127d81be18ba452d34002
Justin.Mihklep <justinmihklep@msn.com>
2023-01-06 10:55:51 +0200

    GUI ready

--- View.py
+++ View.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import tkinter.font as tkfont
+from PIL import Image, ImageTk
 
 
 class View(Tk):
@@ -9,11 +11,26 @@ class View(Tk):
         self.model = model
         self.userinput = StringVar()
 
-        # TODO Fonts
+        # Fonts
+        self.big_font_style = tkfont.Font(family='Courier', size=18, weight='bold')
+        self.default_style_bold = tkfont.Font(family='Verdana', size=10, weight='bold')
+        self.default_style = tkfont.Font(family='Verdana', size=10)
+
         # Window properties
         self.geometry('515x200')
         self.title('Hangman')
         self.center(self)
+
+        # Create two frames
+        self.frame_top, self.frame_bottom, self.frame_image = self.create_two_frames()
+
+        self.image = ImageTk.PhotoImage(Image.open(self.model.image_files[len(self.model.image_files) - 1]))
+        self.label_image = None
+        # Create all Buttons
+        self.btn_new, self.btn_cancel, self.btn_send = self.create_all_buttons()
+        self.lbl_error, self.lbl_time, self.lbl_result = self.create_all_labels()
+
+        self.char_input = self.create_input_entry()
 
     def main(self):
         self.mainloop()
@@ -36,3 +53,56 @@ class View(Tk):
         y = win.winfo_screenheight() // 2 - win_height // 2
         win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
         win.deiconify()
+
+    def create_two_frames(self):
+        frame_top = Frame(self, bg='#BDB76B', height=50)  # gold
+        frame_bottom = Frame(self, bg='#6B8E23')  # green
+
+        frame_top.pack(fill=BOTH, )
+        frame_bottom.pack(expand=True, fill=BOTH)
+
+        # Hangman image frame
+        frame_img = Frame(frame_top, bg='white', width=130, height=130)
+        frame_img.grid(row=0, column=3, rowspan=4, padx=5, pady=5)
+
+        return frame_top, frame_bottom, frame_img  # Method returns two objects
+
+    def create_all_buttons(self):
+        #
+        btn_new = Button(self.frame_top, text='New Game', font=self.default_style)
+        Button(self.frame_top, text='Leaderboard', font=self.default_style).grid(row=0, column=1, padx=5, pady=2,
+                                                                                 sticky=EW)
+
+        # Cancel and Send buttons
+        btn_cancel = Button(self.frame_top, text='Cancel', font=self.default_style, state='disabled')
+        btn_send = Button(self.frame_top, text='Send', font=self.default_style, state='disabled')
+        # Three buttons on frame
+        btn_new.grid(row=0, column=0, padx=5, pady=2, sticky=EW)
+        btn_cancel.grid(row=0, column=2, padx=5, pady=2, sticky=EW)
+        btn_send.grid(row=1, column=2, padx=5, pady=2, sticky=EW)
+
+        return btn_new, btn_send, btn_cancel
+
+    def create_all_labels(self):
+        Label(self.frame_top, text='Input Letter', font=self.default_style_bold).grid(row=1, column=0,
+                                                                                      padx=5, pady=2)
+        lbl_error = Label(self.frame_top, text='Wrong 0 letter(s)', anchor='w',
+                          font=self.default_style_bold)
+        lbl_time = Label(self.frame_top, text='0:00:00', font=self.default_style)
+        lbl_result = Label(self.frame_bottom, text='Let\'s play'.upper(), font=self.big_font_style)
+        # image label
+        self.label_image = Label(self.frame_image, image=self.image)
+        self.label_image.pack()
+
+        lbl_error.grid(row=2, column=0, columnspan=3, sticky=EW, padx=5, pady=2)
+        lbl_time.grid(row=3, column=0, columnspan=3, sticky=EW, padx=5, pady=2)
+        lbl_result.pack(padx=5, pady=2)
+
+        return lbl_error, lbl_time, lbl_result
+
+    def create_input_entry(self):
+        char_input = Entry(self.frame_top, textvariable=self.userinput, justify='center',
+                           font=self.default_style)
+        char_input['state'] = 'disabled'
+        char_input.grid(row=1, column=1, padx=5, pady=2)
+        return char_input
